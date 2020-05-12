95ea8e6a3243c2b25649d878b5e52a53cb7b7f41
yamakawa8 <shinnosuke@pcc.co.jp>
2020-05-12 15:34:09 +0900

    プロジェクト ファイルを追加します。

---
+++ try_github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace try_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
